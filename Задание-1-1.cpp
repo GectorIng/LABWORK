eb5a421f2ca5c91200126662b90d8661f3fa8573
Gector <81628778+GectorIng@users.noreply.github.com>
2021-06-27 00:05:10 +0300

    Add files via upload

---
+++ Задание-1-1.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+const int K = 10;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int arr[K];
+
+	for (int i = 0; i < K; i++)
+	{
+		cout << "Введите элемент " << i + 1 << endl;
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < K; i++)
+		if (arr[i] % 2 == 0) arr[i] = 0;
+
+	for (int i = 0; i < K; i++)
+		cout << arr[i] << " ";
+
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
