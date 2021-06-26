8bc914d85de1b7fdb37c60c0b4d14d4ec4faede3
Gector <81628778+GectorIng@users.noreply.github.com>
2021-06-27 00:14:44 +0300

    Add files via upload

---
+++ LAB-1/Задание-1-2.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	char a;
+	cout << "Введите искомый символ" << endl;
+	cin >> a;
+
+	string b;
+	cout << "Введите строку" << endl;
+	cin >> b;
+
+	bool flag = false;
+
+	for (int i = 0; i < b.length(); i++)
+		if (b[i] == a) { cout << i + 1 << endl; flag = true; break; }
+	
+
+	if (!flag) cout << "Символ отсутсвует" << endl;
+
+	system("pause");
+	return 0;
+}
---
+++ LAB-1/Задание-1-3.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+const int K = 3;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int arr[K][K];
+
+	for (int i = 0; i < K; i++)
+		for (int j = 0; j < K; j++)
+		{
+			cout << "Введите элемент " << i + 1 << " " << j + 1 << endl;
+			cin >> arr[i][j];
+		}
+
+	cout << "Определитель = " << (arr[0][0] * arr[1][1] * arr[2][2] + arr[2][0] * arr[0][1] * arr[1][2] + arr[1][0] * arr[2][1] * arr[0][2] - arr[2][0] * arr[1][1] * arr[0][2] - arr[0][0] * arr[2][1] * arr[1][2] - arr[1][0] * arr[0][1] * arr[2][2]) << endl;
+
+	system("pause");
+	return 0;
+}
