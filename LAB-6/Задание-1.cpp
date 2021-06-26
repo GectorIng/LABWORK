4e518c9612302d2ca2585be29899cf1c8a4218f5
Gector <81628778+GectorIng@users.noreply.github.com>
2021-06-27 01:23:06 +0300

    Add files via upload

---
+++ LAB-6/Задание-1.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int max3(int a, int b, int c);
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	cout << max3(5, 15, 10) << endl;
+
+	system("pause");
+	return 0;
+}
+
+int max3(int a, int b, int c)
+{
+	if (c > a && c > b) return c;
+	if (b > a && b > c) return b;
+	else return a;
+}
---
+++ LAB-6/Задание-2.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int menu(int x, int up, int down);
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	cout << menu(20, 15, 5) << endl;
+
+	system("pause");
+	return 0;
+}
+
+int menu(int x, int up, int down)
+{
+	int flag = 1;
+
+	while (1)
+	{
+		if (x < up && x > down || flag == 0) break;
+
+		cout << "1 - Новое вводное значение" << endl;
+		cout << "2 - Новый нижний предел" << endl;
+		cout << "3 - Новый верхний предел" << endl;
+		cout << "0 - выход" << endl;
+		cin >> flag;
+
+		switch (flag)
+		{
+		case 1: { cin >> x; break; }
+		case 2: { cin >> down; break;  }
+		case 3: { cin >> up; break; }
+		}
+	}
+
+	return x;
+}
